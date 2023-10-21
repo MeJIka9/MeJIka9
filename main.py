5dae035f6c4be611a353bd6330f9d982ebebcea5
MeJIka9 <120459196+MeJIka9@users.noreply.github.com>
2023-10-21 23:39:22 +1000

    Треуголникиииии

--- main.py
+++ main.py
@@ -1,16 +1,16 @@
 i = 0 #перемнная триггер
 while i == 0: #создание цикла
     try: #проверка на текст
-        first = float(input()) #три принимающие переменные
-        second = float(input())
-        third = float(input())
+        a = float(input()) #три принимающие переменные
+        b = float(input())
+        c = float(input())
 #проверка на условие задачи
-        if first + second > third and first > 0 and second > 0 and third > 0:
+        if a + b > c and a > 0 and b > 0 and c > 0:
 #логика программы
-            func1 = (first + second + third) / 2
+            p = (a + b + c) / 2
 
-            result = (func1 * (func1 - first) * (func1 - second) * (func1 - third)) ** 0.5
-            print(result) #вывод результата
+            S = (p * (p - a) * (p - b) * (p - c)) ** 0.5
+            print(S) #вывод результата
             i = i + 1 #закрытие тригера для приостановки цикла
         else:
             print('Введите правильные данные')
